244da5f6fe108fbad1f76c9840bbd8785065db75
andrey.chumak <andrexx@gmail.com>
2011-12-13 23:47:26 +0200

    fix few resharper warnings and remove unused dataset references

--- BlogEngine/Github.BlogEngine/Repositories/ArchiveRepository.cs
+++ BlogEngine/Github.BlogEngine/Repositories/ArchiveRepository.cs
@@ -3,48 +3,47 @@ using System.Collections.Generic;
 using Github.API;
 using Github.API.Model;
 using Github.BlogEngine.Models;
-using System.Linq;
-using System.Linq.Expressions;
 
 namespace Github.BlogEngine.Repositories
 {
-    // not used.. yet
-    public class ArchiveRepository : Github.BlogEngine.Repositories.IArchiveRepository
-    {
-        private Adapter GitAdapter
-        {
-            get { return new Adapter(); }
-        }
-        private static CommitObject _lastCommit = null;
-        private CommitObject LastCommit
-        {
-            get
-            {
-                return _lastCommit ?? GitAdapter.GetAllCommits(GitHubUserSettings.UserName, GitHubUserSettings.Repository,
-                                                               GitHubUserSettings.Branch)[0];
-            }
-        }
+	// not used.. yet
+	public class ArchiveRepository : IArchiveRepository
+	{
+		private Adapter GitAdapter
+		{
+			get { return new Adapter(); }
+		}
 
-        public IList<GitBaseObject> GetYearFolder()
-        {
-            var trees = GitAdapter.GetTrees(GitHubUserSettings.UserName, GitHubUserSettings.Repository, LastCommit.TreeSha);
-            var rootObjects = Utilities.GetGitTreesByType(trees, "tree");
-            return rootObjects;
-        }
+		private static CommitObject _lastCommit;
 
-        public IList<GitBaseObject> GetMonthPostsFolder(string parentSha)
-        {
-            var trees = GitAdapter.GetTrees(GitHubUserSettings.UserName, GitHubUserSettings.Repository, parentSha);
-            var subFolders = Utilities.GetGitTreesByType(trees,"tree");
-            return subFolders;
-        }    
+		private CommitObject LastCommit
+		{
+			get
+			{
+				return _lastCommit ?? GitAdapter.GetAllCommits(GitHubUserSettings.UserName, GitHubUserSettings.Repository,
+				                                               GitHubUserSettings.Branch)[0];
+			}
+		}
 
-        public IList<GitBaseObject> GetMonthPosts(string monthSha)
-        {
-            var files = GitAdapter.GetTrees(GitHubUserSettings.UserName, GitHubUserSettings.Repository, monthSha);
-            var contentFiles = Utilities.GetGitTreeByMimeType(files, "text/html");
-            return contentFiles;
-        }
+		public IList<GitBaseObject> GetYearFolder()
+		{
+			var trees = GitAdapter.GetTrees(GitHubUserSettings.UserName, GitHubUserSettings.Repository, LastCommit.TreeSha);
+			var rootObjects = Utilities.GetGitTreesByType(trees, "tree");
+			return rootObjects;
+		}
 
-    }
+		public IList<GitBaseObject> GetMonthPostsFolder(string parentSha)
+		{
+			var trees = GitAdapter.GetTrees(GitHubUserSettings.UserName, GitHubUserSettings.Repository, parentSha);
+			var subFolders = Utilities.GetGitTreesByType(trees, "tree");
+			return subFolders;
+		}
+
+		public IList<GitBaseObject> GetMonthPosts(string monthSha)
+		{
+			var files = GitAdapter.GetTrees(GitHubUserSettings.UserName, GitHubUserSettings.Repository, monthSha);
+			var contentFiles = Utilities.GetGitTreeByMimeType(files, "text/html");
+			return contentFiles;
+		}
+	}
 }
--- KyivBeerNCode.Web/Controllers/HomeController.cs
+++ KyivBeerNCode.Web/Controllers/HomeController.cs
@@ -1,20 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace KyivBeerNCode.Web.Controllers
 {
-    public class HomeController : Controller
-    {
-        //
-        // GET: /Home/
-
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-    }
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
 }
