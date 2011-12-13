ab8d1bc9466d319e9e07808a04e880dff309d0c8
andrey.chumak <andrexx@gmail.com>
2011-12-13 23:36:50 +0200

    Add extension from SO for selecting active menu item

---
+++ KyivBeerNCode.Web/Infrastructure/ExtensionMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace KyivBeerNCode.Web.Infrastructure
+{
+	public static class ExtensionMethods
+	{
+		public static MvcHtmlString MenuLink(
+			this HtmlHelper htmlHelper,
+			string linkText,
+			string actionName,
+			string controllerName)
+		{
+			string currentAction = htmlHelper.ViewContext.RouteData.GetRequiredString("action");
+			string currentController = htmlHelper.ViewContext.RouteData.GetRequiredString("controller");
+			if (actionName == currentAction && controllerName == currentController)
+			{
+				return htmlHelper.ActionLink(
+					linkText,
+					actionName,
+					controllerName,
+					null,
+					new
+						{
+							@class = "active"
+						});
+			}
+			return htmlHelper.ActionLink(linkText, actionName, controllerName);
+		}
+	}
+}
